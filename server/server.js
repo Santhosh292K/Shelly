dcd0f6d238e21f3b7d4ea7d295d1fcfabe236587
Santhosh A S <santhosh292k@gmail.com>
2025-07-08 23:34:13 +0530

    Created a template

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const uri = process.env.MONGO_URI;
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('✅ MongoDB connected successfully'))
+.catch(err => console.error('❌ MongoDB connection error:', err));
